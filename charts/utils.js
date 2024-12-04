dee10d48720807d12d055c0d8dd29e2adc0333ed
NaelFares <frs.nael@gmail.com>
2024-12-04 23:19:16 +0100

    add configuration URL
    Ajout du fichier de configuration des URL pour les fichiers Json

--- charts/utils.js
+++ charts/utils.js
@@ -217,21 +217,6 @@ function getAll(isArray=false) {
     }
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Choisir quel fichier JSON utiliser
-function getUrlByContinent(continent) {
-    switch (continent) {
-        case "Amérique":
-            return "../data/survey_results_NA.json";
-        case "Europe":
-            return "../data/survey_results_WE.json";
-        default:
-            console.error("Continent invalide. Veuillez choisir 'Amérique' ou 'Europe'.");
-            return null;
-    }
-}
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Trier les documents par pays
